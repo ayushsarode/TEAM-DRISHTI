b24267dd8384a810e200037448bc08633926e454
Pradhunyagawande01 <pradhumnya.gawande.cyb@ghrietn.raisoni.net>
2024-08-24 01:42:45 +0530

    home page

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,18 +1,26 @@
 import React from 'react'
-import CarouselComponent from '../components/Carousel'
+import Carousel from '../components/Carousel'
 import Navbar from '../components/Navbar'
-
 
 const Home = () => {
   return (
     <>
-    <Navbar/>
-<div className='w-[56rem] h-[40rem] mt-10 mx-auto mb-10'>
-
-      <CarouselComponent/>
+      <Navbar />
+      <div className="container mx-auto px-4 py-12 h-screen flex items-center">
+        <div className="grid grid-cols-1 md:grid-cols-2 gap-8 w-full">
+          <div className="flex flex-col justify-center text-center md:text-left">
+            <h1 className="text-4xl font-bold text-gray-800 mb-4">
+              Welcome to Study Nest
+            </h1>
+            <span className="text-lg text-gray-600">
+              We are here to help you in your studies
+            </span>
+          </div>
+          <div className="flex justify-center">
+            <Carousel className="w-full" />
+          </div>
+        </div>
       </div>
-
-      
     </>
   )
 }
