a5f60c26c652e32de75ef7563ca57405f8facd92
swaranjali01 <swaragawande26@gmail.com>
2024-08-23 18:02:42 +0530

    images added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,19 +1,8 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from 'react'
 
-function App() {
-  const [count, setCount] = useState(0)
-
+const App = () => {
   return (
-    <>
-      <div>
-      <h1 className="text-3xl text-red-500 font-bold underline">
-      Hello world!
-    </h1>
-       </div>
-    </>
+    <div>App</div>
   )
 }
 
