53916bbfbde4732c40de72172266e8140d2c3abc
Ayush Sarode <72145248+AyushSarode@users.noreply.github.com>
2024-08-23 20:12:01 +0530

    added

--- server/routes/userRoute.js
+++
@@ -1,9 +0,0 @@
-// routes/auth.js
-const express = require('express');
-const router = express.Router();
-const { signUp, login } = require('../controllers/userController');
-
-router.post('/signup', signUp);
-router.post('/login', login);
-
-module.exports = router;
